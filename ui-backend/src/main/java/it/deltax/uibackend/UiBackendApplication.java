7e4fccbe573a1c547e414248677d94d91d9e0cba
MicheGit <chrimiche@hotmail.it>
2022-01-09 22:45:38 +0100

    spring init

---
+++ ui-backend/src/main/java/it/deltax/uibackend/UiBackendApplication.java
@@ -0,0 +1,13 @@
+package it.deltax.uibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiBackendApplication.class, args);
+	}
+
+}
---
+++ ui-backend/src/test/java/it/deltax/uibackend/UiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.deltax.uibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
