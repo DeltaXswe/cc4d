b3a7fc82745eb5bb36a1419ee6ea28ce8f39827a
MicheGit <chrimiche@hotmail.it>
2022-01-12 12:11:30 +0100

    test funzionante

--- ui-backend/src/test/java/it/deltax/uibackend/UiBackendApplicationTests.java
+++ ui-backend/src/test/java/it/deltax/uibackend/UiBackendApplicationTests.java
@@ -2,20 +2,50 @@ package it.deltax.uibackend;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import it.deltax.uibackend.db.model.Macchina;
+import it.deltax.uibackend.db.repos.MacchinaRepository;
 import it.deltax.uibackend.web.MachinesController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK
+)
+@AutoConfigureMockMvc
 class UiBackendApplicationTests {
 
 	@Autowired
 	private MachinesController machineController;
 
+	@Autowired
+	private MacchinaRepository macchinaRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
 	void contextLoads() {
 		assertThat(machineController).isNotNull();
 	}
+
+	@Test
+	public void listAllMacchine() throws Exception {
+		macchinaRepository.save(new Macchina(1, "One"));
+
+		mockMvc.perform(get("/macchine"))
+				.andDo(print())
+				.andExpect(status().isOk());
+
+	}
+
 
 }
