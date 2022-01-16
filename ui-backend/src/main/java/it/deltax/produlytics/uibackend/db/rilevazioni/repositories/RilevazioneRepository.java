580befec924a0320e79eebbabf04fbc2b7b78252
MicheGit <chrimiche@hotmail.it>
2022-01-16 10:28:58 +0100

    dio mio spring tiret ensema

--- ui-backend/src/main/java/it/deltax/produlytics/uibackend/db/rilevazioni/repositories/RilevazioneRepository.java
+++ ui-backend/src/main/java/it/deltax/produlytics/uibackend/db/rilevazioni/repositories/RilevazioneRepository.java
@@ -2,6 +2,7 @@ package it.deltax.produlytics.uibackend.db.rilevazioni.repositories;
 
 import it.deltax.produlytics.persistence.rilevazioni.Rilevazione;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,11 +13,12 @@ import java.util.List;
 public interface RilevazioneRepository extends CrudRepository<Rilevazione, Long> {
 
     // autoimplementazione da parte di spring
-    List<Rilevazione> findByMacchinaAndCaratteristica(long macchina, String caratteristica, Sort sort);
+    @Query("select r from Rilevazione r where r.macchina = ?1 and r.caratteristica = ?2 order by creazione_utc")
+    List<Rilevazione> findByMacchinaAndCaratteristica(long macchina, String caratteristica);
 
+    @Query("select r from Rilevazione r where r.macchina = :macchina and r.caratteristica = :caratteristica and r.creazioneUtc > :creazione_utc order by creazione_utc")
     List<Rilevazione> findByMacchinaAndCaratteristicaAndCreazioneUtcGreaterThan(
             @Param("macchina") long macchina,
             @Param("caratteristica") String caratteristica,
-            @Param("creazione_utc") long creazioneUtc,
-            Sort sort);
+            @Param("creazione_utc") long creazioneUtc);
 }
