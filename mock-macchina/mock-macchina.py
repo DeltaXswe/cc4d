e4b2cdd1451b0d0a56729e253518221519c8b9d4
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-01-19 13:42:49 +0100

    Aggiungi mock macchina

---
+++ mock-macchina/mock-macchina.py
@@ -0,0 +1,28 @@
+from numpy.random import default_rng
+import requests
+import time
+
+# Configurazione
+# media e varianza sono calcolati dai limiti, assumendo che:
+#  - la media è a metà tra i limiti
+#  - i limiti sono distanti dalla media di 3 * sigma 
+url = "http://api-macchina:8080/detections"
+machine = 1
+characteristic = 1
+limite_min = 100
+limite_max = 300
+
+# Codice
+rng = default_rng()
+mu = (limite_max + limite_min) / 2
+sigma = (limite_max - limite_min) / 6
+while True:
+    time.sleep(1)
+    value = rng.normal(mu, sigma)
+    json = {
+        'createdAtUtc': int(time.time()),
+        'machine': machine,
+        'characteristic': characteristic,
+        'value': value
+    }
+    requests.post(url, data = json)
