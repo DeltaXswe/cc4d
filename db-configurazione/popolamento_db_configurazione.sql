c6daf3991f15b165649695491d7db6afcf4a8e4c
Alberto Lazari <alberto.lazari@gmail.com>
2022-01-24 15:46:01 +0100

    Aggiunta nuova macchina e caratteristiche

--- db-configurazione/popolamento_db_configurazione.sql
+++ db-configurazione/popolamento_db_configurazione.sql
@@ -1,5 +1,8 @@
 insert into macchina (nome) values
-('Macchina a vapore');
+('Macchina a vapore'),
+('Telaio a vapore');
 
 insert into caratteristica (codice, nome, macchina, limite_min, limite_max, media, adattamento, ampiezza_campione) values
-('frequenza_di_oscillazione_pistone', 'Frequenza di oscillazione pistone', 1, 100, 300, 200, true, 0);
+('frequenza_di_oscillazione_pistone', 'Frequenza di oscillazione pistone', 1, 100, 300, 200, true, 0),
+('temperatura_dell_acqua', "Temperatura dell'acqua", 1, 100, 300, 200, true, 0),
+('frequenza_di_produzione_tessuto', 'Frequenza di produzione tessuto', 2, 1, 10, 5, true, 0);
