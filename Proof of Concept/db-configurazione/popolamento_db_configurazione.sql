aab7d8d71621f5fc560ce10f4b52bc24489795df
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-02-09 18:02:32 +0100

    Mock invia a tutte le macchine

--- Proof of Concept/db-configurazione/popolamento_db_configurazione.sql
+++ Proof of Concept/db-configurazione/popolamento_db_configurazione.sql
@@ -5,4 +5,4 @@ insert into macchina (nome) values
 insert into caratteristica (codice, nome, macchina, limite_min, limite_max, media, adattamento, ampiezza_campione) values
 ('frequenza_di_oscillazione_pistone', 'Frequenza di oscillazione pistone', 1, 100, 300, 200, true, 0),
 ('temperatura_dell_acqua', 'Temperatura dell''acqua (°C)', 1, 100, 300, 200, true, 0),
-('frequenza_di_produzione_tessuto', 'Frequenza di produzione tessuto', 2, 1, 10, 5, true, 0);
+('frequenza_di_produzione_tessuto', 'Frequenza di produzione tessuto', 2, 0, 10, 5, true, 0);
--- Proof of Concept/mock-macchina/mock-macchina.py
+++ Proof of Concept/mock-macchina/mock-macchina.py
@@ -3,26 +3,28 @@ import requests
 import time
 
 # Configurazione
-# media e varianza sono calcolati dai limiti, assumendo che:
+# media e varianza sono calcolati da "min" e "max", assumendo che:
 #  - la media è a metà tra i limiti
 #  - i limiti sono distanti dalla media di 3 * sigma 
 url = "http://api:8081/detections"
-machine = 1
-characteristic = "frequenza_di_oscillazione_pistone"
-limite_min = 100
-limite_max = 300
+machine_characteristics = [
+    { "machine": 1, "characteristic": "frequenza_di_oscillazione_pistone", "min": 100, "max": 300 },
+    { "machine": 1, "characteristic": "temperatura_dell_acqua", "min": 100, "max": 300 },
+    { "machine": 2, "characteristic": "frequenza_di_produzione_tessuto", "min": 0, "max": 10 }
+]
 
 # Codice
 rng = default_rng()
-mu = (limite_max + limite_min) / 2
-sigma = (limite_max - limite_min) / 6
 time.sleep(3) # Evita di crashare, anche se c'è il restart automatico
 while True:
     time.sleep(1)
-    value = rng.normal(mu, sigma)
-    json = {
-        'machine': machine,
-        'characteristic': characteristic,
-        'value': value
-    }
-    requests.post(url, json = json)
+    for mc in machine_characteristics:
+        mu = (mc["max"] + mc["min"]) / 2
+        sigma = (mc["max"] - mc["min"]) / 6
+        value = rng.normal(mu, sigma)
+        json = {
+            'machine': mc["machine"],
+            'characteristic': mc["characteristic"],
+            'value': value
+        }
+        requests.post(url, json = json)
