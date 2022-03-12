22ac02acc7d18811f256f41f1e8abf78d39f68bd
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-03-12 12:03:06 +0100

    Semplifica ControlChart7SameOrder

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/business/domain/control_chart/ControlChart7SameOrder.java
+++ Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/business/domain/control_chart/ControlChart7SameOrder.java
@@ -14,23 +14,14 @@ public class ControlChart7SameOrder implements ControlChart {
 
 		List<Detection> last7Detections = lastDetections.subList(lastDetections.size() - 7, lastDetections.size());
 
-		// TODO: Questo algoritmo è orribile
 		int prevCmpResult = 0;
 		for(int i = 0; i < 6; i++) {
 			Detection prev = last7Detections.get(i);
 			Detection next = last7Detections.get(i + 1);
-
-			int cmpResult = 0;
-			if(prev.value() < next.value()) {
-				cmpResult = 1;
-			} else if(prev.value() > next.value()) {
-				cmpResult = -1;
-			}
-
+			int cmpResult = Double.compare(prev.value(), next.value());
 			if(prevCmpResult == -cmpResult) {
 				return;
 			}
-
 			prevCmpResult = cmpResult;
 		}
 
