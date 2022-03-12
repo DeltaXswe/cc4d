e42d3f42cb1f39784d7c01fa231444bfb20cc2c6
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-03-12 18:33:35 +0100

    Rimuovi query inutile

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/repositories/CharacteristicRepository.java
+++ Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/repositories/CharacteristicRepository.java
@@ -2,23 +2,6 @@ package it.deltax.produlytics.api.repositories;
 
 import it.deltax.produlytics.persistence.CharacteristicEntity;
 import it.deltax.produlytics.persistence.CharacteristicEntityId;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
-public interface CharacteristicRepository extends CrudRepository<CharacteristicEntity, CharacteristicEntityId> {
-	// TODO: Change query
-	@Query("SELECT "
-		+ "machine.api_key AS apiKey, "
-		+ "device.archived AS deviceArchived, "
-		+ "device.deactivated AS deviceDeactivated, "
-		+ "characteristic.archived AS characteristicArchived "
-		+ "FROM characteristic "
-		+ "JOIN device ON characteristic.device_id = device.characteristicId "
-		+ "WHERE characteristic.characteristicId = :characteristicId")
-	Optional<ValidationInfoProjection> findValidationInfo(
-		@Param("apiKey") String apiKey, @Param("characteristicId") int charateristicId
-	);
-}
+public interface CharacteristicRepository extends CrudRepository<CharacteristicEntity, CharacteristicEntityId> {}
