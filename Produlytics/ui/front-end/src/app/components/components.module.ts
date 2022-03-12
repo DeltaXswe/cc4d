79bdebbc638413bc358b7c918c3b7f9fe25d14e6
MicheGit <chrimiche@hotmail.it>
2022-03-12 12:57:59 +0100

    add shared component

---
+++ Produlytics/ui/front-end/src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConfirmDialogComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
