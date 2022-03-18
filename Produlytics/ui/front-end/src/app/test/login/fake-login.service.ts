6d9b4ab4946f33b73a51f0002b5a08db40936a84
MicheGit <chrimiche@hotmail.it>
2022-03-18 19:42:51 +0100

    webstorm toglie gli spazi perché è ocd

--- Produlytics/ui/front-end/src/app/test/login/fake-login.service.ts
+++ Produlytics/ui/front-end/src/app/test/login/fake-login.service.ts
@@ -10,7 +10,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 
 export class FakeLoginService implements LoginAbstractService {
-  
+
   private accounts: Login[] = [
     {
       username: 'Gianni',
@@ -21,13 +21,18 @@ export class FakeLoginService implements LoginAbstractService {
       username: 'Cosimo',
       isAdmin: false,
       password: 'Cosimo'
+    },
+    {
+      username: 'deltax',
+      isAdmin: true,
+      password: 'deltax'
     }
   ]
 
   constructor(public router: Router, private matSnackBar: MatSnackBar) { }
 
     public login(user: string, pw: string): Observable<any> {
-      if(this.accounts.find(account => account.username === user && 
+      if(this.accounts.find(account => account.username === user &&
         this.accounts.find(account => account.password === pw))){
         localStorage.setItem('accessToken', JSON.stringify(
           this.accounts.find(wow => wow.username === user))
@@ -38,8 +43,8 @@ export class FakeLoginService implements LoginAbstractService {
       this.matSnackBar.open('Credenziali non valide', 'Undo', {
         duration: 3000
       });
-      return throwError('Credenziali non valide');  
-    }  
+      return throwError('Credenziali non valide');
+    }
 
   public isLogged(): boolean {
     if (localStorage.getItem('accessToken'))
@@ -66,6 +71,6 @@ export class FakeLoginService implements LoginAbstractService {
     if (user)
       return JSON.parse(user).username;
     else
-      return 'username'; 
+      return 'username';
   }
 }
