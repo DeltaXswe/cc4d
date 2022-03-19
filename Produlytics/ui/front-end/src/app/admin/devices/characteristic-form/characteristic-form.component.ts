b4993935fdabb1893b74f4f11c94269e40023ffa
MicheGit <chrimiche@hotmail.it>
2022-03-19 18:58:40 +0100

    component

--- Produlytics/ui/front-end/src/app/admin/devices/characteristic-form/characteristic-form.component.ts
+++ Produlytics/ui/front-end/src/app/admin/devices/characteristic-form/characteristic-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Characteristic} from "../../../model/admin-device/characteristic/characteristic";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-characteristic-form',
@@ -6,10 +9,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characteristic-form.component.css']
 })
 export class CharacteristicFormComponent implements OnInit {
+  @Input()
+  startingData: Characteristic | null = null;
 
-  constructor() { }
+  formGroup: FormGroup;
 
-  ngOnInit(): void {
+  public readonly duplicateNameBehavior = new BehaviorSubject<true | null>(null);
+
+  constructor(
+    formBuilder: FormBuilder
+  ) {
+    this.formGroup = formBuilder.group({
+      name: new FormControl('', Validators.required),
+      autoAdjust: new FormControl(false),
+      upperLimit: new FormControl(''),
+      lowerLimit: new FormControl(''),
+      sampleSize: new FormControl('')
+    });
   }
 
+  ngOnInit(): void {
+    this.formGroup.get('autoAdjust')?.valueChanges.subscribe(selected => {
+      if (selected) {
+        this.formGroup.get('upperLimit')?.removeValidators(Validators.required);
+        this.formGroup.get('upperLimit')?.updateValueAndValidity();
+        this.formGroup.get('lowerLimit')?.removeValidators(Validators.required);
+        this.formGroup.get('lowerLimit')?.updateValueAndValidity();
+      } else {
+        this.formGroup.get('upperLimit')?.setValidators(Validators.required);
+        this.formGroup.get('upperLimit')?.updateValueAndValidity();
+        this.formGroup.get('lowerLimit')?.setValidators(Validators.required);
+        this.formGroup.get('lowerLimit')?.updateValueAndValidity();
+      }
+    });
+    if (this.startingData) {
+      this.formGroup.setValue({
+        name: this.startingData.name,
+        autoAdjust: this.startingData.autoAdjust,
+        upperLimit: this.startingData.upperLimit,
+        lowerLimit: this.startingData.lowerLimit,
+        sampleSize: this.startingData.sampleSize
+      });
+    }
+    this.duplicateNameBehavior.subscribe(isError => {
+      this.formGroup.get('name')?.setErrors({
+        duplicateCharacteristicName: isError
+      });
+      this.formGroup.updateValueAndValidity();
+    });
+  }
+
+  public requireData(): any {
+    const rawValue = this.formGroup.getRawValue();
+    if (rawValue.autoAdjust) {
+      rawValue.upperLimit = null;
+      rawValue.lowerLimit = null;
+    } else {
+      rawValue.sampleSize = null;
+    }
+    return rawValue;
+  }
 }
