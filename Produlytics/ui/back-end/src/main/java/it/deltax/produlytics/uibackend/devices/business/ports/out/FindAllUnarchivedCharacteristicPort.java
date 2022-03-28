32cfdfc6a7bc30c3fc2d9c75265f083da6369a8a
Alberto Lazari <alberto.lazari@gmail.com>
2022-03-28 09:36:54 +0200

    Adattato comportamento alle query

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/business/ports/out/FindAllUnarchivedCharacteristicPort.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/business/ports/out/FindAllUnarchivedCharacteristicPort.java
@@ -3,8 +3,7 @@ package it.deltax.produlytics.uibackend.devices.business.ports.out;
 import it.deltax.produlytics.uibackend.devices.business.domain.CharacteristicTitle;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FindAllUnarchivedCharacteristicPort {
-    Optional<List<CharacteristicTitle>> findAllByDeviceId(int deviceId);
+    List<CharacteristicTitle> findAllByDeviceId(int deviceId);
 }
