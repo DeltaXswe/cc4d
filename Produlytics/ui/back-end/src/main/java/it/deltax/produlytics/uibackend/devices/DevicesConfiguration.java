b2ba4558fa30414ebce4e9d5042e0f2b01bfd618
Alberto Lazari <alberto.lazari@gmail.com>
2022-03-28 12:04:17 +0200

    Tests

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/DevicesConfiguration.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/DevicesConfiguration.java
@@ -2,13 +2,10 @@ package it.deltax.produlytics.uibackend.devices;
 
 import it.deltax.produlytics.uibackend.devices.business.FindCharacteristicInfoService;
 import it.deltax.produlytics.uibackend.devices.business.ListAllDevicesService;
-import it.deltax.produlytics.uibackend.devices.business.GetUnarchivedCharacteristicsService;
 import it.deltax.produlytics.uibackend.devices.business.ports.in.ListAllDevicesUseCase;
-import it.deltax.produlytics.uibackend.devices.business.ports.in.GetUnarchivedCharacteristicsUseCase;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.FindCharacteristicPort;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.FindMachinePort;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.ListAllDevicesPort;
-import it.deltax.produlytics.uibackend.devices.business.ports.out.FindAllUnarchivedCharacteristicPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,13 +15,6 @@ public class DevicesConfiguration {
     @Bean
 	ListAllDevicesUseCase listAllDevicesUseCase(ListAllDevicesPort port) {
         return new ListAllDevicesService(port);
-    }
-
-    @Bean
-	GetUnarchivedCharacteristicsUseCase listCharacteristicsUseCase(
-        FindAllUnarchivedCharacteristicPort port
-    ) {
-        return new GetUnarchivedCharacteristicsService(port);
     }
 
     @Bean
---
+++ Produlytics/ui/back-end/src/test/java/it/deltax/produlytics/uibackend/CharacteristicsTests.java
@@ -0,0 +1,82 @@
+package it.deltax.produlytics.uibackend;
+
+import it.deltax.produlytics.persistence.CharacteristicEntity;
+import it.deltax.produlytics.persistence.CharacteristicEntityId;
+import it.deltax.produlytics.uibackend.devices.web.CharacteristicsController;
+import it.deltax.produlytics.uibackend.repositories.UnarchivedCharacteristicRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.MOCK
+)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class CharacteristicsTests {
+
+	@Autowired
+	private CharacteristicsController characteristicsController;
+
+	@Autowired
+	private UnarchivedCharacteristicRepository unarchivedCharacteristicRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	@Test
+	void contextLoads() {
+		assertThat(characteristicsController).isNotNull();
+	}
+
+	@Test
+	void getUnarchivedCharacteristics() throws Exception {
+		unarchivedCharacteristicRepository.save(new CharacteristicEntity(
+			new CharacteristicEntityId(1, 1),
+			"char",
+			100d,
+			10d,
+			true,
+			0,
+			false
+		));
+		unarchivedCharacteristicRepository.save(new CharacteristicEntity(
+			new CharacteristicEntityId(1, 2),
+			"char2",
+			100d,
+			10d,
+			true,
+			0,
+			false
+		));
+
+		mockMvc.perform(get("/devices/1/characteristics"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	void getUnarchivedCharacteristicsError() throws Exception {
+		unarchivedCharacteristicRepository.save(new CharacteristicEntity(
+			new CharacteristicEntityId(1, 1),
+			"char",
+			100d,
+			10d,
+			true,
+			0,
+			false
+		));
+
+		mockMvc.perform(get("/devices/2/characteristics"))
+			.andDo(print())
+			.andExpect(status().isNotFound());
+	}
+}
