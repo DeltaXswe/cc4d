ba22c876bc1889bc80b38a758c1de7325eabfcd6
Alberto Lazari <alberto.lazari@gmail.com>
2022-03-29 08:47:49 +0200

    Rebase

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/DevicesConfiguration.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/DevicesConfiguration.java
@@ -2,13 +2,10 @@ package it.deltax.produlytics.uibackend.devices;
 
 import it.deltax.produlytics.uibackend.devices.business.FindCharacteristicInfoService;
 import it.deltax.produlytics.uibackend.devices.business.GetUnarchivedDevicesService;
-import it.deltax.produlytics.uibackend.devices.business.GetUnarchivedCharacteristicsService;
 import it.deltax.produlytics.uibackend.devices.business.ports.in.GetUnarchivedDevicesUseCase;
-import it.deltax.produlytics.uibackend.devices.business.ports.in.GetUnarchivedCharacteristicsUseCase;
+import it.deltax.produlytics.uibackend.devices.business.ports.out.FindAllUnarchivedDevicesPort;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.FindCharacteristicPort;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.FindUnarchivedDevicePort;
-import it.deltax.produlytics.uibackend.devices.business.ports.out.FindAllUnarchivedDevicesPort;
-import it.deltax.produlytics.uibackend.devices.business.ports.out.FindAllUnarchivedCharacteristicPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
