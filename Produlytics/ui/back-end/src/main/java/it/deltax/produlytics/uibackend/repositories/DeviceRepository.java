d0aeb29922f5acde709c07397c430efd14b903e8
Leila <leila.dardouri@outlook.it>
2022-03-29 18:52:42 +0200

    .

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/repositories/DeviceRepository.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/repositories/DeviceRepository.java
@@ -1,14 +1,8 @@
 package it.deltax.produlytics.uibackend.repositories;
 
-import it.deltax.produlytics.persistence.CharacteristicEntity;
 import it.deltax.produlytics.persistence.DeviceEntity;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface DeviceRepository extends CrudRepository<DeviceEntity, Integer> {
--- Produlytics/ui/back-end/src/test/java/it/deltax/produlytics/uibackend/AdminTest.java
+++ Produlytics/ui/back-end/src/test/java/it/deltax/produlytics/uibackend/AdminTest.java
@@ -1,23 +1,21 @@
 package it.deltax.produlytics.uibackend;
 
 import it.deltax.produlytics.persistence.AccountEntity;
-import it.deltax.produlytics.persistence.CharacteristicEntity;
-import it.deltax.produlytics.persistence.CharacteristicEntityId;
+import it.deltax.produlytics.persistence.DeviceEntity;
 import it.deltax.produlytics.uibackend.admins.web.AdminsController;
-import it.deltax.produlytics.uibackend.devices.web.CharacteristicsController;
 import it.deltax.produlytics.uibackend.repositories.AdminRepository;
-import it.deltax.produlytics.uibackend.repositories.UnarchivedCharacteristicRepository;
+import it.deltax.produlytics.uibackend.repositories.DeviceRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import org.springframework.http.ResponseEntity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class AdminTest extends UiBackendApplicationTests {
@@ -26,6 +24,9 @@ public class AdminTest extends UiBackendApplicationTests {
 
 	@Autowired
 	private AdminRepository adminRepository;
+
+	@Autowired
+	private DeviceRepository deviceRepository;
 
 	@Override
 	@Test
@@ -98,4 +99,33 @@ public class AdminTest extends UiBackendApplicationTests {
 			.andExpect(status().isNotFound());
 	}
 
+	@Test
+	public void testModifyDevice() throws Exception {
+		deviceRepository.save(new DeviceEntity("Macchina1", false, false, ""));
+
+		mockMvc.perform(put("/admin/devices/1/name")
+				.param("name", "Macchina2")
+			).andDo(print())
+			.andExpect(status().isNoContent());
+	}
+
+	@Test
+	public void testModifyDeviceNotFound() throws Exception {
+		deviceRepository.save(new DeviceEntity("Macchina1", false, false, ""));
+
+		mockMvc.perform(put("/admin/devices/100/name")
+				.param("name", "Macchina2")
+			).andDo(print())
+			.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void testModifyDeviceArchStatus() throws Exception {
+		deviceRepository.save(new DeviceEntity("Macchina1", false, false, ""));
+
+		mockMvc.perform(put("/admin/devices/1/archived")
+				.param("archived", "true")
+			).andDo(print())
+			.andExpect(status().isNoContent());
+	}
 }
