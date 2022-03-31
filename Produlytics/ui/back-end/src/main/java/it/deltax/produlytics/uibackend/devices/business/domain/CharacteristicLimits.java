88622a45192d8357dcc313b563502cacbc7bfae8
Alberto Lazari <alberto.lazari@gmail.com>
2022-03-31 23:26:18 +0200

    Static factory

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/business/domain/CharacteristicLimits.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/business/domain/CharacteristicLimits.java
@@ -1,9 +1,23 @@
 package it.deltax.produlytics.uibackend.devices.business.domain;
 
-import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
 
 public record CharacteristicLimits(
 	double lowerLimit,
 	double upperLimit,
-	OptionalDouble mean
-) {}
+	double mean
+) {
+	public static CharacteristicLimits newCharacteristicLimits(double lowerLimit, double upperLimit) {
+		return new CharacteristicLimits(
+			lowerLimit,
+			upperLimit,
+			mean(lowerLimit, upperLimit)
+		);
+	}
+
+	private static double mean(double lowerLimit, double upperLimit) {
+		return DoubleStream.of(lowerLimit, upperLimit)
+			.average()
+			.getAsDouble();
+	}
+}
