91b82a08843736e735c5eabbb74f7fb250b63bcd
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-04-01 16:27:46 +0200

    Fixa indentazione documentazione

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/business/domain/control_chart/Utils.java
+++ Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/business/domain/control_chart/Utils.java
@@ -8,11 +8,11 @@ public class Utils {
 	// Ritorna uno stream di "finestre" di larghezza `size` data una lista `list`. Ad esempio, dato:
 	//   list = [1, 2, 3, 4, 5, 6, 7], size = 3
 	// vengono restituiti:
-	//        [1, 2, 3]
-	//           [2, 3, 4]
-	//              [3, 4, 5]
-	//                 [4, 5, 6]
-	//                    [5, 6, 7]
+	//          [1, 2, 3]
+	//             [2, 3, 4]
+	//                [3, 4, 5]
+	//                   [4, 5, 6]
+	//                      [5, 6, 7]
 	public static <T> Stream<List<T>> windows(List<T> list, int size) {
 		return IntStream.range(0, list.size() - size + 1).mapToObj(i -> list.subList(i, i + size));
 	}
