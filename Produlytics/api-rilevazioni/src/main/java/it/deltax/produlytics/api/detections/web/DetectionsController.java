f4bc410339a4947af6ce31ebd98f2fa07deb47bc
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-04-01 18:38:31 +0200

    Setta il codice di stato HTTP

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/web/DetectionsController.java
+++ Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/web/DetectionsController.java
@@ -4,6 +4,7 @@ import it.deltax.produlytics.api.detections.business.domain.IncomingDetection;
 import it.deltax.produlytics.api.detections.business.ports.in.ProcessIncomingDetectionUseCase;
 import it.deltax.produlytics.api.exceptions.BusinessException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,9 @@ public class DetectionsController {
 	@Autowired
 	private ProcessIncomingDetectionUseCase processIncomingDetectionUseCase;
 
-	// TODO: Gestisci il codice HTTP e gli errori
 	@PostMapping("")
-	public void addDetection(@RequestBody IncomingDetection incomingDetection) throws BusinessException {
+	public HttpStatus addDetection(@RequestBody IncomingDetection incomingDetection) throws BusinessException {
 		processIncomingDetectionUseCase.processIncomingDetection(incomingDetection);
+		return HttpStatus.ACCEPTED;
 	}
 }
