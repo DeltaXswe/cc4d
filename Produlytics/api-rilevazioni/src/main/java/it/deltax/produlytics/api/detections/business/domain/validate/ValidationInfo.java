8a82b9202a4fc125d751ac41b587e9a326c8d332
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-04-04 14:15:13 +0200

    Rimosso ValidationInfo

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/detections/business/domain/validate/ValidationInfo.java
+++
@@ -1,5 +0,0 @@
-package it.deltax.produlytics.api.detections.business.domain.validate;
-
-// Dati ritornati dal validatore delle richieste.
-// Per ora include solo l'identificativo della macchina ottenuto a partire dalla chiave API.
-public record ValidationInfo(int deviceId) {}
