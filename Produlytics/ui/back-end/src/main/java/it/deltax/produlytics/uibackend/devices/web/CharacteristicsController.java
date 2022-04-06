db0853afb34c441c2be8d7878d5e3be37560c551
Leila <leila.dardouri@outlook.it>
2022-04-06 09:18:08 +0200

    correzioni rebase

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/web/CharacteristicsController.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/web/CharacteristicsController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/devices/{id}/characteristics")
 public class CharacteristicsController {
