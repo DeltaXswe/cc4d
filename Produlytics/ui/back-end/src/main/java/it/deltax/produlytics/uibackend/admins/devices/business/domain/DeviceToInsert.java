41dfd314f817bbab39c0d8365164c424ebf7a9e2
Leila <leila.dardouri@outlook.it>
2022-04-07 11:46:01 +0200

    .

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/domain/DeviceToInsert.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/domain/DeviceToInsert.java
@@ -1,10 +1,10 @@
 package it.deltax.produlytics.uibackend.admins.devices.business.domain;
 
-import it.deltax.produlytics.uibackend.devices.business.domain.DetailedCharacteristic;
+import it.deltax.produlytics.uibackend.admins.characteristics.business.domain.NewCharacteristic;
 
 import java.util.List;
 
 public record DeviceToInsert(
 	String name,
-	List<DetailedCharacteristic> characteristics
+	List<NewCharacteristic> characteristics
 ) {}
