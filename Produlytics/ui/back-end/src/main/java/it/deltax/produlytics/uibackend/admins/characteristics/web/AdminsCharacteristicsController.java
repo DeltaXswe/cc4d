d19ef6b78df32c3acf35e65b4fa9888993b6ae84
Alberto Lazari <alberto.lazari@gmail.com>
2022-04-07 10:42:59 +0200

    Request body

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/characteristics/web/AdminsCharacteristicsController.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/characteristics/web/AdminsCharacteristicsController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
 
 @RestController
 @RequestMapping("/admins/devices/{deviceId}/characteristics")
@@ -22,26 +20,11 @@ public class AdminsCharacteristicsController {
 	@PostMapping("")
 	public ResponseEntity<Map<String, Integer>> insertCharacteristic(
 		@PathVariable("deviceId") int deviceId,
-		@RequestParam(value = "name") String name,
-		@RequestParam(value = "lowerLimit", required = false) Double lowerLimit,
-		@RequestParam(value = "upperLimit", required = false) Double upperLimit,
-		@RequestParam(value = "autoAdjust") boolean autoAdjust,
-		@RequestParam(value = "sampleSize", required = false) Integer sampleSize
+		@RequestBody NewCharacteristic characteristic
 	) throws BusinessException {
-		var builder = NewCharacteristic.toBuilder()
-			.withName(name)
-			.withAutoAdjust(autoAdjust);
-
-		if (lowerLimit != null)
-			builder = builder.withLowerLimit(OptionalDouble.of(lowerLimit));
-		if (upperLimit != null)
-			builder = builder.withUpperLimit(OptionalDouble.of(upperLimit));
-		if (sampleSize != null)
-			builder = builder.withSampleSize(OptionalInt.of(sampleSize));
-
 		return ResponseEntity.ok(Map.of(
 			"id",
-			insertCharacteristic.insertByDevice(deviceId, builder.build())
+			insertCharacteristic.insertByDevice(deviceId, characteristic)
 		));
 	}
 }
