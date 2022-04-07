1752434d7550c2d093a2888c8a758a02dcc28770
Leila <leila.dardouri@outlook.it>
2022-04-07 11:46:01 +0200

    .

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/InsertDeviceService.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/InsertDeviceService.java
@@ -1,5 +1,7 @@
 package it.deltax.produlytics.uibackend.admins.devices.business;
 
+import it.deltax.produlytics.uibackend.admins.characteristics.business.domain.NewCharacteristic;
+import it.deltax.produlytics.uibackend.admins.characteristics.business.ports.out.InsertCharacteristicPort;
 import it.deltax.produlytics.uibackend.admins.devices.business.ports.in.InsertDeviceUseCase;
 import it.deltax.produlytics.uibackend.admins.devices.business.domain.DeviceToInsert;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.InsertDevicePort;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class InsertDeviceService implements InsertDeviceUseCase {
 	private InsertDevicePort insertDevicePort;
 	private CreateDevice createDevice;
-	//TODO InsertCharacteristicPort
+	private InsertCharacteristicPort insertCharacteristicPort;
 
 	public InsertDeviceService(InsertDevicePort insertDevicePort, CreateDevice createDevice){
 		this.insertDevicePort = insertDevicePort;
@@ -19,6 +21,8 @@ public class InsertDeviceService implements InsertDeviceUseCase {
 	@Override
 	public int insertDevice(DeviceToInsert device) {
 		int id = insertDevicePort.insertDevice(createDevice.createDevice(device.name()));
+		for(NewCharacteristic characteristic : device.characteristics())
+			insertCharacteristicPort.insertByDevice(id, characteristic);
 
 		return id;
 	}
--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/devices/business/domain/DetailedCharacteristic.java
+++
@@ -1,9 +0,0 @@
-package it.deltax.produlytics.uibackend.devices.business.domain;
-
-public record DetailedCharacteristic(
-	int id,
-	double lowerLimit,
-	double upperLimit,
-	boolean autoAdjust,
-	int sampleSize
-) {}
