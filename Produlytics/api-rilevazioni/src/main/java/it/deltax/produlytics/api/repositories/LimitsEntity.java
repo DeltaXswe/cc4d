1ce5358b262d62010432c7a80fba47d25277b4c2
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-04-08 19:26:37 +0200

    Rimuovi TODO: non è possibile

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/repositories/LimitsEntity.java
+++ Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/repositories/LimitsEntity.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 // Rappresenta i limiti "grezzi" di una caratteristica.
 public interface LimitsEntity {
-	// TODO: Vedere se Spring accetta OptionalDouble invece che Optional<Double>
 	boolean getAutoAdjust();
 	Optional<Double> getTechnicalLowerLimit();
 	Optional<Double> getTechnicalUpperLimit();
