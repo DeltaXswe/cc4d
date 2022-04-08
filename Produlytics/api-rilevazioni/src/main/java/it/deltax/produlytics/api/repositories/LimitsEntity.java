6aa593e20ada614ef8d1a466e1e214fef7d243fa
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-04-08 15:03:24 +0200

    boolean per getAutoAdjust

--- Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/repositories/LimitsEntity.java
+++ Produlytics/api-rilevazioni/src/main/java/it/deltax/produlytics/api/repositories/LimitsEntity.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 // Rappresenta i limiti "grezzi" di una caratteristica.
 public interface LimitsEntity {
 	// TODO: Vedere se Spring accetta OptionalDouble invece che Optional<Double>
-	Boolean getAutoAdjust();
+	boolean getAutoAdjust();
 	Optional<Double> getTechnicalLowerLimit();
 	Optional<Double> getTechnicalUpperLimit();
 	double getComputedMean();
