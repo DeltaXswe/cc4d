512ca921244d3ecbf21a70d3e650c90bb7ea6059
Alberto Lazari <alberto.lazari@gmail.com>
2022-04-08 10:39:21 +0200

    ErrorTest not working

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/exceptions/exceptions/BusinessException.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/exceptions/exceptions/BusinessException.java
@@ -2,10 +2,9 @@ package it.deltax.produlytics.uibackend.exceptions.exceptions;
 
 import it.deltax.produlytics.uibackend.exceptions.ErrorType;
 
-import java.util.function.Supplier;
-
 public class BusinessException extends Exception {
-private final ErrorType type;
+	private final ErrorType type;
+
 	public BusinessException(String code, ErrorType type) {
 		super(code);
 		this.type = type;
