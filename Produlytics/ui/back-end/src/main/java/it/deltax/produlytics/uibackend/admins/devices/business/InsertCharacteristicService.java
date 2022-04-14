0a50a25e9fe0042e0a8b04599bfc7ff293ca9aa1
Leila Dardouri <leila.dardouri@outlook.it>
2022-04-14 19:53:06 +0200

    use case non port

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/InsertCharacteristicService.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/InsertCharacteristicService.java
@@ -2,9 +2,8 @@ package it.deltax.produlytics.uibackend.admins.devices.business;
 
 import it.deltax.produlytics.uibackend.admins.devices.business.domain.CharacteristicConstraintsToCheck;
 import it.deltax.produlytics.uibackend.admins.devices.business.domain.NewCharacteristic;
-import it.deltax.produlytics.uibackend.admins.devices.business.ports.in.InsertCharacteristicUseCase;
 import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.FindDetailedCharacteristicPort;
-import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.InsertCharacteristicPort;
+import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.InsertCharacteristicUseCase;
 import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.FindDetailedDevicePort;
 import it.deltax.produlytics.uibackend.exceptions.ErrorType;
 import it.deltax.produlytics.uibackend.exceptions.BusinessException;
@@ -14,8 +13,8 @@ import org.springframework.stereotype.Service;
  * Il service per l'insierimento di una nuova caratteristica
  */
 @Service
-public class InsertCharacteristicService implements InsertCharacteristicUseCase {
-	private final InsertCharacteristicPort insertPort;
+public class InsertCharacteristicService implements it.deltax.produlytics.uibackend.admins.devices.business.ports.in.InsertCharacteristicUseCase {
+	private final InsertCharacteristicUseCase insertPort;
 	private final FindDetailedDevicePort findDevicePort;
 	private final FindDetailedCharacteristicPort findCharacteristicPort;
 
@@ -26,7 +25,7 @@ public class InsertCharacteristicService implements InsertCharacteristicUseCase 
 	 * @param findCharacteristicPort la porta per trovare una caratteristica
 	 */
 	InsertCharacteristicService(
-		InsertCharacteristicPort insertPort,
+		InsertCharacteristicUseCase insertPort,
 		FindDetailedDevicePort findDevicePort,
 		FindDetailedCharacteristicPort findCharacteristicPort
 	) {
--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/InsertDeviceService.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/InsertDeviceService.java
@@ -1,7 +1,7 @@
 package it.deltax.produlytics.uibackend.admins.devices.business;
 
 import it.deltax.produlytics.uibackend.admins.devices.business.domain.NewCharacteristic;
-import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.InsertCharacteristicPort;
+import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.InsertCharacteristicUseCase;
 import it.deltax.produlytics.uibackend.admins.devices.business.ports.in.InsertDeviceUseCase;
 import it.deltax.produlytics.uibackend.admins.devices.business.domain.DeviceToInsert;
 import it.deltax.produlytics.uibackend.admins.devices.business.ports.out.InsertDevicePort;
@@ -14,22 +14,22 @@ import org.springframework.stereotype.Service;
 public class InsertDeviceService implements InsertDeviceUseCase {
 	private final InsertDevicePort insertDevicePort;
 	private final CreateDevice createDevice;
-	private final InsertCharacteristicPort insertCharacteristicPort;
+	private final InsertCharacteristicUseCase insertCharacteristicUseCase;
 
 
 	/**
 	 * Il costruttore
 	 * @param insertDevicePort la porta per inserire una macchina
 	 * @param createDevice crea una nuova macchina
-	 * @param insertCharacteristicPort la porta per inserire una caratteristica
+	 * @param insertCharacteristicUseCase la porta per inserire una caratteristica
 	 */
 	public InsertDeviceService(
 		InsertDevicePort insertDevicePort,
 		CreateDevice createDevice,
-		InsertCharacteristicPort insertCharacteristicPort){
+		InsertCharacteristicUseCase insertCharacteristicUseCase){
 		this.insertDevicePort = insertDevicePort;
 		this.createDevice = createDevice;
-		this.insertCharacteristicPort = insertCharacteristicPort;
+		this.insertCharacteristicUseCase = insertCharacteristicUseCase;
 	}
 
 
@@ -42,7 +42,7 @@ public class InsertDeviceService implements InsertDeviceUseCase {
 	public int insertDevice(DeviceToInsert device) {
 		int id = this.insertDevicePort.insertDevice(this.createDevice.createDevice(device.name()));
 		for(NewCharacteristic characteristic : device.characteristics())
-			this.insertCharacteristicPort.insertByDevice(id, characteristic);
+			this.insertCharacteristicUseCase.insertByDevice(id, characteristic);
 		return id;
 	}
 }
