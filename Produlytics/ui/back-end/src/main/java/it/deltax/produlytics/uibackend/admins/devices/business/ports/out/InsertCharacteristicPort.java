73817c62bb9801b9242b3746e29b3aa6723cbee6
Alberto Lazari <alberto.lazari@gmail.com>
2022-04-17 11:53:02 +0200

    -public

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/ports/out/InsertCharacteristicPort.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/admins/devices/business/ports/out/InsertCharacteristicPort.java
@@ -3,5 +3,5 @@ package it.deltax.produlytics.uibackend.admins.devices.business.ports.out;
 import it.deltax.produlytics.uibackend.admins.devices.business.domain.NewCharacteristic;
 
 public interface InsertCharacteristicPort {
-	public int insertByDevice(int deviceId, NewCharacteristic characteristic);
+	int insertByDevice(int deviceId, NewCharacteristic characteristic);
 }
