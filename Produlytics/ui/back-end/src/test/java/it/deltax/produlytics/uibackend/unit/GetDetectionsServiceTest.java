7bc5adebf8985dc529a30fd143e3065b9000edc8
Alberto Lazari <alberto.lazari@gmail.com>
2022-04-18 11:27:05 +0200

    DetectionsTest

--- Produlytics/ui/back-end/src/test/java/it/deltax/produlytics/uibackend/unit/GetDetectionsServiceTest.java
+++ Produlytics/ui/back-end/src/test/java/it/deltax/produlytics/uibackend/unit/GetDetectionsServiceTest.java
@@ -8,6 +8,8 @@ import it.deltax.produlytics.uibackend.detections.business.services.GetDetection
 import it.deltax.produlytics.uibackend.devices.business.domain.CharacteristicLimits;
 import it.deltax.produlytics.uibackend.devices.business.ports.out.FindCharacteristicLimitsPort;
 import it.deltax.produlytics.uibackend.exceptions.BusinessException;
+import it.deltax.produlytics.uibackend.exceptions.ErrorType;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -75,11 +77,119 @@ public class GetDetectionsServiceTest {
 		assert detections.nextNew() == 3;
 	}
 
+	@Test
+	void testGetDetectionsWithOlderThan() throws BusinessException {
+		service = new GetDetectionsService(
+			new FindAllDetectionsWithOlderThanPortMock(),
+			new FindCharacteristicLimitsPortMock()
+		);
+
+		DetectionsGroup detections = service.listByCharacteristic(
+			1,
+			1,
+			DetectionFilters.builder()
+				.withOlderThan(OptionalLong.of(3))
+				.build()
+		);
+
+		assert detections.detections().equals(List.of(detection1, detection2));
+		assert detections.nextOld().isEmpty();
+		assert detections.nextNew() == 2;
+	}
+
+	@Test
+	void testGetDetectionsWithNewerThan() throws BusinessException {
+		service = new GetDetectionsService(
+			new FindAllDetectionsPortMock(),
+			new FindCharacteristicLimitsPortMock()
+		);
+
+		DetectionsGroup detections = service.listByCharacteristic(
+			1,
+			1,
+			DetectionFilters.builder()
+				.withNewerThan(OptionalLong.of(1))
+				.build()
+		);
+
+		assert detections.detections().equals(List.of(detection2, detection3));
+		assert detections.nextOld().getAsLong() == 2;
+		assert detections.nextNew() == 3;
+	}
+
+	@Test
+	void testGetEmptyDetections() throws BusinessException {
+		service = new GetDetectionsService(
+			new FindAllDetectionsEmptyPortMock(),
+			new FindCharacteristicLimitsPortMock()
+		);
+
+		DetectionsGroup detections = service.listByCharacteristic(
+			1,
+			1,
+			DetectionFilters.builder()
+				.build()
+		);
+
+		assert detections.detections().equals(Collections.emptyList());
+		assert detections.nextOld().isEmpty();
+	}
+
+	@Test
+	void testGetEmptyByFiltersDetections() throws BusinessException {
+		service = new GetDetectionsService(
+			new FindAllDetectionsPortMock(),
+			new FindCharacteristicLimitsPortMock()
+		);
+
+		DetectionsGroup detections = service.listByCharacteristic(
+			1,
+			1,
+			DetectionFilters.builder()
+				.withOlderThan(OptionalLong.of(1))
+				.withNewerThan(OptionalLong.of(3))
+				.build()
+		);
+
+		assert detections.detections().equals(Collections.emptyList());
+		assert detections.nextOld().isEmpty();
+	}
+
+	@Test
+	void testGetNotFoundAndArchived() {
+		service = new GetDetectionsService(
+			new FindAllDetectionsPortMock(),
+			new FindCharacteristicLimitsNotFoundPortMock()
+		);
+
+		BusinessException exception = assertThrows(BusinessException.class, () -> service.listByCharacteristic(
+			1,
+			1,
+			DetectionFilters.builder()
+				.build()
+		));
+		assert exception.getMessage().equals("characteristicNotFound");
+		assert exception.getType() == ErrorType.NOT_FOUND;
+	}
+
 	// Classi Mock
 	private static class FindAllDetectionsPortMock implements FindAllDetectionsPort {
 		@Override
 		public List<Detection> findAllByCharacteristic(int deviceId, int characteristicId, OptionalLong olderThan) {
 			return List.of(detection3, detection2, detection1);
+		}
+	}
+
+	private static class FindAllDetectionsWithOlderThanPortMock implements FindAllDetectionsPort {
+		@Override
+		public List<Detection> findAllByCharacteristic(int deviceId, int characteristicId, OptionalLong olderThan) {
+			List<Detection> list = new ArrayList<>(List.of(detection3, detection2, detection1));
+
+			while (!(list.get(0).creationTime() < olderThan.getAsLong())) {
+				list.remove(0);
+			}
+
+			return list;
 		}
 	}
 
