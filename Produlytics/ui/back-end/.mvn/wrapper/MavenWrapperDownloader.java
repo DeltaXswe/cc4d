4440e256450386babf9f2015870b9faa8def5262
Alberto Lazari <alberto.lazari@gmail.com>
2022-04-21 13:23:28 +0200

    Format strano

--- Produlytics/ui/back-end/.mvn/wrapper/MavenWrapperDownloader.java
+++ Produlytics/ui/back-end/.mvn/wrapper/MavenWrapperDownloader.java
@@ -95,7 +95,7 @@ public class MavenWrapperDownloader {
     }
 
     private static void downloadFileFromURL(String urlString, File destination) throws Exception {
-        if(System.getenv("MVNW_USERNAME") != null && System.getenv("MVNW_PASSWORD") != null) {
+        if (System.getenv("MVNW_USERNAME") != null && System.getenv("MVNW_PASSWORD") != null) {
             String username = System.getenv("MVNW_USERNAME");
             char[] password = System.getenv("MVNW_PASSWORD").toCharArray();
             Authenticator.setDefault(new Authenticator() {
