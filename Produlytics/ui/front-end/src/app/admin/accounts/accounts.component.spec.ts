438190a7e8873ad63c9521277b1d0ffc30af57a8
MicheGit <chrimiche@hotmail.it>
2022-05-02 23:34:35 +0200

    Am I testing my code... or is it testing me?

--- Produlytics/ui/front-end/src/app/admin/accounts/accounts.component.spec.ts
+++ Produlytics/ui/front-end/src/app/admin/accounts/accounts.component.spec.ts
@@ -4,8 +4,9 @@ import { AccountsComponent } from './accounts.component';
 import {LoginAbstractService} from "../../model/login/login-abstract.service";
 import {FakeAccountService} from "../../test/account/fake-account.service";
 import {AccountAbstractService} from "../../model/admin-account/account-abstract.service";
-import {MockDialog, testModules} from "../../test/utils";
+import {MockDialogAlwaysConfirm, testModules} from "../../test/utils";
 import {MatDialog} from "@angular/material/dialog";
+import {aliceUser, bobUser, cosimoUser, users} from "../../test/account/users";
 
 describe('AccountsComponent', () => {
   let component: AccountsComponent;
@@ -14,9 +15,7 @@ describe('AccountsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AccountsComponent ],
-      imports: [
-        ...testModules
-      ],
+      imports: testModules,
       providers: [
         FakeAccountService,
         {
@@ -27,14 +26,21 @@ describe('AccountsComponent', () => {
           provide: LoginAbstractService,
           useExisting: FakeAccountService
         },
+        MockDialogAlwaysConfirm,
         {
           provide: MatDialog,
-          useExisting: MockDialog
+          useExisting: MockDialogAlwaysConfirm
         }
       ]
     })
     .compileComponents();
     fixture = TestBed.createComponent(AccountsComponent);
+    const loginService = TestBed.inject(LoginAbstractService);
+    await loginService.login({
+      username: aliceUser.username,
+      password: aliceUser.password,
+      rememberMe: false
+    }).toPromise();
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -42,4 +48,39 @@ describe('AccountsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('apri-nuovo-utente', (doneFn) => {
+    component.openNewAccountDialog();
+    component.accounts.connect().subscribe(() => {
+      doneFn();
+    });
+  });
+
+  it('apri-modifica-utente', (doneFn) => {
+    component.openNewAccountDialog();
+    component.accounts.connect().subscribe(doneFn);
+  });
+
+  it('archivia-utente', (doneFn) => {
+    component.toggleStatus(cosimoUser);
+    component.accounts.connect().subscribe(data => {
+      if (cosimoUser.archived) {
+        doneFn();
+      }
+    });
+  });
+
+  it('ripristina-utente', (doneFn) => {
+    component.toggleStatus(bobUser);
+    component.accounts.connect().subscribe(data => {
+      if (!bobUser.archived) {
+        doneFn();
+      }
+    });
+  });
+
+  it('utente-loggato', () => {
+    expect(component.isLoggedUser(aliceUser)).toBeTrue();
+    expect(component.isLoggedUser(cosimoUser)).toBeFalse();
+  })
 });
