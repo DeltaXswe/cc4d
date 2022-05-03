059d9e05d9351a82cee14654af53d48cbd9cd03e
riccardopavan <ricky1640@gmail.com>
2022-05-03 22:12:05 +0200

    alcuni test

--- Produlytics/ui/front-end/src/app/main/login/login.component.spec.ts
+++ Produlytics/ui/front-end/src/app/main/login/login.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AccountAbstractService } from 'src/app/model/admin-account/account-abstract.service';
+import { LoginAbstractService } from 'src/app/model/login/login-abstract.service';
+import { FakeAccountService } from 'src/app/test/account/fake-account.service';
+import { testModules } from 'src/app/test/utils';
+import { gianniUser } from 'src/app/test/account/users';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +14,21 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [
+        RouterTestingModule,
+        testModules
+      ],
+      declarations: [ LoginComponent ],
+      providers: [
+        {
+          provide: AccountAbstractService,
+          useExisting: FakeAccountService
+        },
+        {
+          provide: LoginAbstractService,
+          useExisting: FakeAccountService
+        }
+      ]
     })
     .compileComponents();
   });
@@ -18,8 +38,41 @@ describe('LoginComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('validità campi obbligatori', () => {
+    let username = component.loginForm.controls['username'];
+    let password = component.loginForm.controls['password'];
+    expect(username.valid).toBeFalsy();
+    expect(password.valid).toBeFalsy();
+    expect(component.loginForm.valid).toBeFalsy();
+
+    username.setValue('');
+    password.setValue('');
+    
+    expect(username.hasError('required')).toBeTruthy();
+    expect(password.hasError('required')).toBeTruthy();
+  })
+
+  it('validità campo password>6caratteri', () => {
+    let password = component.loginForm.controls['password'];
+
+    password.setValue('ciao');
+
+    expect(component.loginForm.valid).toBeFalsy();
+    expect(password.hasError('minlength')).toBeTruthy();
+  })
+
+  it('form valido con input validi', () => {
+    let username = component.loginForm.controls['username'];
+    let password = component.loginForm.controls['password'];
+
+    username.setValue(gianniUser.username);
+    password.setValue(gianniUser.password);
+
+    expect(component.loginForm.valid).toBeTruthy();
+  })
 });
--- Produlytics/ui/front-end/src/app/main/modify-pw/modify-pw.component.spec.ts
+++ Produlytics/ui/front-end/src/app/main/modify-pw/modify-pw.component.spec.ts
@@ -1,14 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AccountAbstractService } from 'src/app/model/admin-account/account-abstract.service';
+import { LoginAbstractService } from 'src/app/model/login/login-abstract.service';
+import { ModifyPwAbstractService } from 'src/app/model/modify-pw/modify-pw-abstract.service';
+import { FakeAccountService } from 'src/app/test/account/fake-account.service';
+import { MockDialogRef, testModules } from 'src/app/test/utils';
 
 import { ModifyPwComponent } from './modify-pw.component';
 
-describe('ModifyPwComponent', () => {
+fdescribe('ModifyPwComponent', () => {
   let component: ModifyPwComponent;
   let fixture: ComponentFixture<ModifyPwComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModifyPwComponent ]
+      imports: [
+        RouterTestingModule,
+        testModules
+      ],
+      declarations: [ ModifyPwComponent ],
+      providers: [
+        {
+          provide: ModifyPwAbstractService,
+          useExisting: FakeAccountService
+        },
+        {
+          provide: LoginAbstractService,
+          useExisting: FakeAccountService
+        },
+        MockDialogRef,
+        {
+          provide: MatDialogRef,
+          useExisting: MockDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: null
+        },
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +51,54 @@ describe('ModifyPwComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('validità campi obbligatori', () => {
+    let form = component.modifyPw;
+    expect(form.valid).toBeFalsy();
+
+    form.controls['oldPassword'].setValue('');
+    form.controls['newPassword'].setValue('');
+    form.controls['newPasswordRe'].setValue('');
+    expect(form.controls['oldPassword'].hasError('required')).toBeTruthy();
+    expect(form.controls['newPassword'].hasError('mustBeDifferent')).toBeTruthy();
+    expect(form.controls['newPasswordRe'].hasError('required')).toBeTruthy();
+  });
+
+  it('validita password 6 caratteri', () => {
+    let form = component.modifyPw;
+
+    form.controls['oldPassword'].setValue('Paol');
+    form.controls['newPassword'].setValue('Paolo');
+    form.controls['newPasswordRe'].setValue('Paolo');
+
+    expect(form.controls['newPassword'].hasError('minlength')).toBeTruthy();
+  })
+
+  it('password corrente uguale a quella nuova', () => {
+    let form = component.modifyPw;
+    form.controls['oldPassword'].setValue('Gianni');
+    form.controls['newPassword'].setValue('Gianni');
+    form.controls['newPasswordRe'].setValue('Gianni');
+
+    expect(form.controls['newPassword'].hasError('mustBeDifferent')).toBeTruthy();
+  });
+
+  it('password nuova diversa da ripetizione', () => {
+    let form = component.modifyPw;
+    form.controls['oldPassword'].setValue('Gianni');
+    form.controls['newPassword'].setValue('Giannni');
+    form.controls['newPasswordRe'].setValue('Gianni');
+
+    expect(form.controls['newPasswordRe'].hasError('mismatch')).toBeTruthy();
+  });
+
+  it('campi validi', () => {
+    let form = component.modifyPw;
+    form.controls['oldPassword'].setValue('Gianni');
+    form.controls['newPassword'].setValue('Gianni1');
+    form.controls['newPasswordRe'].setValue('Gianni1');
+
+    expect(form.valid).toBeTruthy();
   });
 });
