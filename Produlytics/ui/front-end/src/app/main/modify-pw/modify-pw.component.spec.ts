8b8c0f7d4f86f731cb56a2945fc64529cff9f496
MicheGit <chrimiche@hotmail.it>
2022-05-04 22:36:37 +0200

    test di unità parte admin vanno

--- Produlytics/ui/front-end/src/app/main/modify-pw/modify-pw.component.spec.ts
+++ Produlytics/ui/front-end/src/app/main/modify-pw/modify-pw.component.spec.ts
@@ -13,7 +13,7 @@ import { MockDialogRef, testModules } from 'src/app/test/utils';
 
 import { ModifyPwComponent } from './modify-pw.component';
 
-fdescribe('ModifyPwComponent', () => {
+describe('ModifyPwComponent', () => {
   let component: ModifyPwComponent;
   let fixture: ComponentFixture<ModifyPwComponent>;
   let mockDialogRef: MatDialogRef<any>;
