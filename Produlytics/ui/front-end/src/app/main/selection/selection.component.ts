4a5c10ff9367cb7b9106232ceda1a3a1693296e9
MicheGit <chrimiche@hotmail.it>
2022-05-15 23:09:14 +0200

    saaaaa

--- Produlytics/ui/front-end/src/app/main/selection/selection.component.ts
+++ Produlytics/ui/front-end/src/app/main/selection/selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { FlatTreeControl } from "@angular/cdk/tree";
 import {
   UnarchivedCharacteristicAbstractService
