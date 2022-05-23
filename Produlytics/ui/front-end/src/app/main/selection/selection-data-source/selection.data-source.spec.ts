593afe74e7be4a9c1355f7d88b3f1ac38e65e3c9
MicheGit <chrimiche@hotmail.it>
2022-05-23 22:25:28 +0200

    questo test non va

--- Produlytics/ui/front-end/src/app/main/selection/selection-data-source/selection.data-source.spec.ts
+++ Produlytics/ui/front-end/src/app/main/selection/selection-data-source/selection.data-source.spec.ts
@@ -1,31 +1,50 @@
 import {SelectionDataSource} from "./selection.data-source";
-import {NEVER, of, Subject} from "rxjs";
+import {NEVER, Subject} from "rxjs";
 import {FlatTreeControl} from "@angular/cdk/tree";
 import {SelectionNode} from "./selection-node";
 import {SelectionChange} from "@angular/cdk/collections";
+import {FakeDeviceService} from "../../../test/device/fake-device.service";
 import {UnarchivedDeviceAbstractService} from "../../../model/device/unarchived-device-abstract.service";
+import {
+  UnarchivedCharacteristicAbstractService
+} from "../../../model/characteristic/unarchived-characteristic-abstract.service";
 
 describe('SelectionDataSource', () => {
+  let dataSource: SelectionDataSource;
+  let changed: Subject<SelectionChange<SelectionNode>>;
+  let treeControl: FlatTreeControl<SelectionNode>;
+  let deviceService: UnarchivedDeviceAbstractService;
+  let characteristicService: UnarchivedCharacteristicAbstractService;
 
-  it('connetti-selezione', () => {
-    const changed = new Subject<SelectionChange<SelectionNode>>();
-    const treeControl: FlatTreeControl<SelectionNode> = {
+  beforeEach(() => {
+    changed = new Subject<SelectionChange<SelectionNode>>();
+    treeControl = {
       expansionModel: {
         changed
       }
     } as FlatTreeControl<SelectionNode>; // trust me, compiler
-    const deviceService: UnarchivedDeviceAbstractService = {
-      getDevices(): Observable<UnarchivedDevice[]> {
-      }
-    };
-    const dataSource = new SelectionDataSource(
+    deviceService = new FakeDeviceService();
+    characteristicService = new FakeDeviceService();
+    dataSource = new SelectionDataSource(
       treeControl,
       deviceService,
       characteristicService
     );
-    dataSource.connect({
+  });
+
+
+  it('connetti-selezione', () => {
+
+    const observable = dataSource.connect({
       viewChange: NEVER
     });
+
+    changed.next()
+
+    observable.subscribe(value => {
+
+    });
+
   });
 
 })
