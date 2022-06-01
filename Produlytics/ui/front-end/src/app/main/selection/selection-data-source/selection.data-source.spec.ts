d1be4284d5fe0b1325ea788f8cf62c42900bbf75
MicheGit <chrimiche@hotmail.it>
2022-06-01 23:26:17 +0200

    unit test datasource

--- Produlytics/ui/front-end/src/app/main/selection/selection-data-source/selection.data-source.spec.ts
+++ Produlytics/ui/front-end/src/app/main/selection/selection-data-source/selection.data-source.spec.ts
@@ -1,30 +1,68 @@
 import {SelectionDataSource} from "./selection.data-source";
-import {NEVER, Subject} from "rxjs";
+import {delay, NEVER, Observable, of, Subject} from "rxjs";
 import {FlatTreeControl} from "@angular/cdk/tree";
 import {SelectionNode} from "./selection-node";
 import {SelectionChange} from "@angular/cdk/collections";
-import {FakeDeviceService} from "../../../test/device/fake-device.service";
+import {FakeDeviceService, locomotivaDevice} from "../../../test/device/fake-device.service";
 import {UnarchivedDeviceAbstractService} from "../../../model/device/unarchived-device-abstract.service";
 import {
   UnarchivedCharacteristicAbstractService
 } from "../../../model/characteristic/unarchived-characteristic-abstract.service";
+import {CharacteristicNode} from "./characteristic-node";
+import {DeviceNode} from "./device-node";
+import {UnarchivedDevice} from "../../../model/device/unarchived-device";
+import {UnarchivedCharacteristic} from "../../../model/characteristic/unarchived-characteristic";
 
 describe('SelectionDataSource', () => {
   let dataSource: SelectionDataSource;
   let changed: Subject<SelectionChange<SelectionNode>>;
   let treeControl: FlatTreeControl<SelectionNode>;
+  let expansionModel: any;
   let deviceService: UnarchivedDeviceAbstractService;
   let characteristicService: UnarchivedCharacteristicAbstractService;
+  let tornioDevice: UnarchivedDevice;
+  let tornioChars: UnarchivedCharacteristic[];
+  let pompaDevice: UnarchivedDevice;
+  let pompaChars: UnarchivedCharacteristic[];
+
 
   beforeEach(() => {
     changed = new Subject<SelectionChange<SelectionNode>>();
-    treeControl = {
-      expansionModel: {
-        changed
+    expansionModel = {
+      changed
+    };
+    treeControl = { expansionModel } as FlatTreeControl<SelectionNode>; // trust me, compiler
+    tornioDevice = {
+      name: 'Tornio',
+      id: 1
+    };
+    tornioChars = [
+      { id: 1, name: 'Revolver' },
+      { id: 2, name: 'Boccole' },
+    ];
+    pompaDevice = {
+      name: 'Pompa a immersione',
+      id: 2
+    };
+    pompaChars = [
+      { id: 1, name: 'Ventola' },
+      { id: 2, name: 'Velocità aspirazione' }
+    ];
+
+    deviceService = {
+      getDevices(): Observable<UnarchivedDevice[]> {
+        return of([tornioDevice, pompaDevice]);
       }
-    } as FlatTreeControl<SelectionNode>; // trust me, compiler
-    deviceService = new FakeDeviceService();
-    characteristicService = new FakeDeviceService();
+    };
+    characteristicService = {
+      getCharacteristicsByDevice(deviceId: number): Observable<UnarchivedCharacteristic[]> {
+        if (deviceId === 1) {
+          return of(tornioChars);
+        } else {
+          return of(pompaChars);
+        }
+      }
+    };
     dataSource = new SelectionDataSource(
       treeControl,
       deviceService,
@@ -33,17 +71,76 @@ describe('SelectionDataSource', () => {
   });
 
 
-  it('connetti-selezione', () => {
+  it('connetti-selezione', (doneFn) => {
+
+    const subject = new Subject<any>();
+
+    const observable = dataSource.connect({
+      viewChange: subject
+    });
+
+    subject.next(true);
+
+    observable.subscribe(value => {
+      expect(value).toEqual([new DeviceNode(tornioDevice), new DeviceNode(pompaDevice)]);
+      doneFn();
+    });
+  });
+
+  it('apri-macchina', (doneFn) => {
 
     const observable = dataSource.connect({
       viewChange: NEVER
     });
 
+    const tornio = (dataSource as any).dataStream.value[0];
 
-    observable.subscribe(value => {
+    changed.next({
+      source: expansionModel,
+      removed: [],
+      added: [tornio]
+    });
 
+    const ids = [tornio, pompaDevice, ...tornioChars].map(node => node.id);
+
+    observable.subscribe(values => {
+      for (const value of values) {
+        expect(ids).toContain(value.id)
+      }
+      doneFn();
     });
 
   });
 
-})
+  it('chiudi-macchina', doneFn => {
+    const observable = dataSource.connect({
+      viewChange: NEVER
+    });
+
+    const tornio = (dataSource as any).dataStream.value[0];
+    const pompa = (dataSource as any).dataStream.value[1];
+
+    changed.next({
+      source: expansionModel,
+      removed: [],
+      added: [tornio, pompa]
+    });
+
+    changed.next({
+      source: expansionModel,
+      removed: [tornio],
+      added: []
+    });
+
+
+    const ids = [tornio, pompaDevice, ...pompaChars].map(node => node.id);
+
+    observable.subscribe(values => {
+      for (const value of values) {
+        expect(ids).toContain(value.id)
+      }
+      doneFn();
+    });
+  });
+
+});
