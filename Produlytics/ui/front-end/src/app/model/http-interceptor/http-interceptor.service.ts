1454b02519bd37c465aea39e2bfa0162db2c1185
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-06-05 11:51:40 +0200

    XhrInterceptor: rimuovi valori nell'header inutili

--- Produlytics/ui/front-end/src/app/model/http-interceptor/http-interceptor.service.ts
+++ Produlytics/ui/front-end/src/app/model/http-interceptor/http-interceptor.service.ts
@@ -26,33 +26,9 @@ export class XhrInterceptor implements HttpInterceptor {
    * @returns Un {@link Observable} della richiesta trasformata
    */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let user = localStorage.getItem('accessToken');
-    if (user) {
-      req = req.clone({
-        headers: req.headers.set('X-Auth-Token', user)
-      });
-    }
     req = req.clone({
       headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
     });
-
-    /**
-     * Spring Boot di default è impostato in modo tale da bloccare le richieste REST che fanno del side effect
-     * (POST, PUT, DELETE, PATCH). Il modo per attivarle è mettere un header che si chiami X-XSFR-TOKEN e valorizzarlo
-     * con un cookie che ci dà il server. Questo è quanto si evince dalla documentazione di Spring Boot e da questo
-     * articolo https://www.baeldung.com/spring-security-csrf#2-front-end-configuration, da cui ho preso questa
-     * soluzione.
-     *
-     * Tutto ciò è parzialmente vero perché all'inizio il server non bloccava le POST, ma solo le PUT. Quindi potrebbe
-     * esserci dietro un bug di Spring Boot che non le impedisce. Resta il fatto che mettendo
-     * queste due linee di codice ora tutto funziona.
-     *
-     * Il cookie non dovrebbe essere HttpOnly, quindi non dovrebbe dare problemi questa get.
-     * */
-    const csrfToken = this.cookieService.get('XSRF-TOKEN');
-    if (csrfToken) {
-      req.headers.set('X-XSRF-TOKEN', csrfToken);
-    }
 
     return next.handle(req);
   }
