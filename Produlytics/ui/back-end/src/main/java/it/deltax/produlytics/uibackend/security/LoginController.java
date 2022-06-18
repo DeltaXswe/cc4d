212ac4777e9a710b98dea223b28e8b399eda3a25
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-06-18 10:34:47 +0200

    Backend: muovi endpoint per i dati dell'utente a /accounts/info

--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/security/LoginController.java
+++ Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/security/LoginController.java
@@ -1,12 +1,5 @@
 package it.deltax.produlytics.uibackend.security;
 
-import it.deltax.produlytics.uibackend.exceptions.BusinessException;
-import it.deltax.produlytics.uibackend.exceptions.ErrorType;
-import java.util.Map;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
   /** Riceve le chiamate all'endpoint REST per l'autenticazione. */
   @GetMapping
-  public SessionInfo login(Authentication authentication) throws BusinessException {
-    if (authentication != null) {
-      var authorities = authentication.getAuthorities();
-      var adminAuthority = ProdulyticsGrantedAuthority.ADMIN.getAuthority();
-      var admin = authorities.stream().anyMatch(auth -> auth.getAuthority().equals(adminAuthority));
-      var username = authentication.getName();
-      return new SessionInfo(username, admin);
-    } else {
-      throw new BusinessException("unauthorized", ErrorType.UNAUTHORIZED);
-    }
-  }
+  public void login() {}
 }
--- Produlytics/ui/back-end/src/main/java/it/deltax/produlytics/uibackend/security/SessionInfo.java
+++
@@ -1,6 +0,0 @@
-package it.deltax.produlytics.uibackend.security;
-
-public record SessionInfo(
-    String username,
-    boolean admin
-) {}
